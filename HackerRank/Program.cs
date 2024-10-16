7f4efc399ce6a952cf3ec177aef74cdb9a2d2241
andrea <monicaab.mtz95@gmail.com>
2024-10-15 23:34:19 -0600

    Repeated Strings

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -2,6 +2,7 @@
 
 using HackerRank.WarmupChanllenges;
 using HackerRank.JumpingontheClouds;
+using HackerRank.RepeatedString;
 using System.Text.RegularExpressions;
 
 #region Counting Valleys
@@ -11,7 +12,7 @@ Console.WriteLine($"Total of Valleys {valley}");
 
 #endregion
 
-
+/*
 Console.WriteLine("\n -------------------------------");
 Console.WriteLine("Jumping on Clouds");
 string path = "0 0 1 0 0 1 0";
@@ -27,4 +28,14 @@ foreach(char cloud in paath)
 int jumps = jumpingOnClouds.jumpingOnTheClouds(clouds);
 
 Console.WriteLine($"Jumps {jumps}");
+Console.ReadKey();
+*/
+
+Console.WriteLine("\n -------------------------------");
+Console.WriteLine("Repeated String");
+
+string s = "cfimaakj";
+long n = 554045874191;
+var result = RepeatedString.repeatedString(s, n);
+Console.WriteLine(result);
 Console.ReadKey();
---
+++ HackerRank/Repeated String/RepeatedString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank.RepeatedString
+{
+	public class RepeatedString
+	{
+        public static long repeatedString(string s, long n)
+        {
+            //    abcac 10
+            long countChar = 0;
+            countChar = s.Count(c => c == 'a') * (n / s.Length);
+            long missedChar = (n % s.Length);
+            for ( long m = 0; m < missedChar; m++)
+            {
+                if (s[(int)m] == 'a')
+                {
+                    countChar += 1;
+                }
+            }
+            return countChar;
+        }
+    }
+}
+
